e64733348714b5bcfd8f411b7b45b17e6578e1b3
tika551 <sindykartikaumar@gmail.com>
2025-02-08 10:13:00 +0800

    OperatorPenugasan.Java

---
+++ OperatorPenugasan.Java
@@ -0,0 +1,35 @@
+public class operatorPenugasan { public static void main(String[] args) {
+
+    int a = 10;
+
+    int b = 5;
+
+    int c = 0;
+
+    c = a + b;
+
+    System.out.println("c = a + b = " + c);
+
+    c = a;
+
+    System.out.println(" c = a = " + c);
+
+    c += a;
+
+    System.out.println(" c += a = " + c);
+
+    c -= a; 
+
+    System.out.println("c -= a =" + c);
+
+    c *= a;
+
+    System.out.println("c *= a =" + c);
+
+
+
+}
+
+    
+
+}
