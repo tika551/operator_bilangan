0008292e7714303bfb9072a9abeeed8a03af094e
tika551 <sindykartikaumar@gmail.com>
2025-02-08 10:09:49 +0800

    OperatorBit.Java

---
+++ OperatorBit.Java
@@ -0,0 +1,13 @@
+public class operatorBit { public static void main(String args) {
+    //contoh : operator AND (&) dan OR (|)
+    int a = 5; 
+    int b = 3; 
+
+    int and = a & b; 
+    int or = a | b; 
+    System.out.println("hasil AND:" + and);
+    System.out.println("hasil OR:" + or);
+
+}
+    
+}
