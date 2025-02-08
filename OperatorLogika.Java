05eea7647ea62f9ad6968928021b423a59868e37
tika551 <sindykartikaumar@gmail.com>
2025-02-08 10:11:49 +0800

    OperatorLogika.Java

---
+++ OperatorLogika.Java
@@ -0,0 +1,27 @@
+public class operatorLogika { public static void main(String[] args) {
+
+    //contoh : operator AND (&&) dan OR (||)
+
+    boolean x = true;
+
+    boolean y = false;
+
+    
+
+    boolean and = x && y;
+
+    boolean or = x || y;
+
+
+
+    System.out.println("hasil AND:" + and);
+
+    System.out.println("hasil OR:" + or);
+
+    
+
+}
+
+    
+
+}
